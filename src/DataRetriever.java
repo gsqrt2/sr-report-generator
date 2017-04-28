140f5de4866f94e6908d54e786e7bc216824b35a
Giorgos Marios <gsqrt2@gmail.com>
2017-04-28 21:58:03 +0300

    with error parsing

--- src/DataRetriever.java
+++ src/DataRetriever.java
@@ -1,10 +1,14 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
+import java.util.Iterator;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 
 public class DataRetriever {
@@ -12,6 +16,26 @@ public class DataRetriever {
 		dataFile = xlsxFile;
 		try{
 			FileInputStream fis = new FileInputStream(dataFile);
+			XSSFWorkbook book = new XSSFWorkbook(fis);
+            XSSFSheet sheet = book.getSheetAt(0);
+            
+          //  Iterator<Row> itr = sheet.iterator();
+            
+         // Iterating over Excel file in Java
+            //while (itr.hasNext()) {
+              //  Row row = itr.next();
+
+                // Iterating over each column of Excel file
+                //Iterator<Cell> cellIterator = row.cellIterator();
+               // (cellIterator.hasNext()) {
+
+                    //Cell cell = cellIterator.next();
+                    //System.out.println(cell.toString());
+                    
+                   
+               // }
+                System.out.println("");
+           // }
 		}
 		catch(IOException ioe)
 		{
