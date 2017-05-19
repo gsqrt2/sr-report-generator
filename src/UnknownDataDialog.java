af1ddd26be3798e2d2ff42544c6e994f0a80cec0
Giorgos Marios <gsqrt2@gmail.com>
2017-05-19 22:20:30 +0300

    building dialog

--- src/UnknownDataDialog.java
+++ src/UnknownDataDialog.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
-import javax.swing.BoxLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 
 public class UnknownDataDialog extends JDialog{
@@ -30,22 +30,55 @@ public class UnknownDataDialog extends JDialog{
 		mainPanel = new JPanel();
 		
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		textPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		addLocationPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		
-		testLabel1 = new JLabel("proti grammi");
-		testLabel2 = new JLabel("deyteri grammi");
-		testLabel3 = new JLabel("triti grammi");
+		ignoreAlwaysPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		ignoreOncePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		textPanel.add(new JLabel("Τί να κάνω με την άγνωστη τοποθεσία;"));
+		
+		buttonGroup = new ButtonGroup();
+	
+		String[] petStrings = { "Bird", "Cat", "Dog", "Rabbit", "Pig" };
+		addRadio = new JRadioButton("Η περιοχή ανήκει στο ΤΤΛΠ: ");
+		ttlpComboBox = new JComboBox(petStrings);
+		tyComboBox = new JComboBox(petStrings);
+		
+		ignoreOnceRadio = new JRadioButton("Αγνόησε μόνο αυτή τη φορά, να ερωτηθώ ξανα");
+		ignoreAlwaysRadio = new JRadioButton("Να αγνοείται η συγκεκριμένη εγγραφή");
+		
+		
+		buttonGroup.add(addRadio);
+		buttonGroup.add(ignoreAlwaysRadio);
+		buttonGroup.add(ignoreOnceRadio);
+				
+		addLocationPanel.add(addRadio);
+		addLocationPanel.add(ttlpComboBox);
+		addLocationPanel.add(new JLabel(", ΤΥ: "));
+		addLocationPanel.add(tyComboBox);
+		//addLocationPanel.add(new JLabel("Η περιοχή ανήκει στο: "));
+		
+		ignoreAlwaysPanel.add(ignoreAlwaysRadio);
+		//ignoreAlwaysPanel.add(new JLabel());
+		
+		ignoreOncePanel.add(ignoreOnceRadio);
+		//ignoreOncePanel.add(new JLabel());
+		
 		okButton = new JButton("Ok");
-		mainPanel.add(testLabel1);
-		mainPanel.add(testLabel2);
-		mainPanel.add(testLabel3);
+		mainPanel.add(textPanel);
+		mainPanel.add(addLocationPanel);
+		mainPanel.add(ignoreAlwaysPanel);
+		mainPanel.add(ignoreOncePanel);
 		mainPanel.add(okButton);
 		this.add(mainPanel);
 		//mainPanel.setPreferredSize(new Dimension(550,250));
-		
+		System.out.println("->"+buttonGroup.getButtonCount());
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
 	}
 	
 	private void addToLocations()
@@ -54,8 +87,10 @@ public class UnknownDataDialog extends JDialog{
 	}
 	
 	private String dataString, dataType;
-	private JPanel mainPanel, addLocationPanel, ignoreAlwaysPanel, ignoreOncePanel;
-	private JLabel testLabel1, testLabel2, testLabel3;
+	private JPanel textPanel, mainPanel, addLocationPanel, ignoreAlwaysPanel, ignoreOncePanel, buttonPanel;
 	private JButton okButton;
 	private DataRetriever parentDataRetriever;
+	private JRadioButton addRadio, ignoreOnceRadio, ignoreAlwaysRadio;
+	private ButtonGroup buttonGroup;
+	private JComboBox ttlpComboBox, tyComboBox;
 }
